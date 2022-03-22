972ef3e71b571fc98a197fcb4720fc26bbf9ae50
Gabriel <nobrecoder@gmail.com>
2022-03-22 14:19:01 -0300

    22/03/2022

---
+++ arquivos_das_aulas/09-strings_como_usar_em_php.php
@@ -0,0 +1,19 @@
+<?php
+
+    //STRIGS PODEM SER USADAS NO PHP DAS SEGUINTES FORMAS:
+
+    $nome = 'Gabriel';
+
+    //Aspas duplas podemos interpolar com variáveis e caracteres especiais...
+    echo "eu uso interpolação $nome\n";
+
+    //Aspa única não pode interpolar com nada...
+    echo 'nao uso interpolação';
+
+    //Caracteres Escape com uso da contra barra...
+    echo "\n"; //Pula uma linha
+    echo "\tFaz tab";
+    echo "\v\vFaz tab vertical";
+    echo "Quebrando linha com "
+
+?>
