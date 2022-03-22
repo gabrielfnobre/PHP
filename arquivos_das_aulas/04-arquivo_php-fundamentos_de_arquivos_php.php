21b04a4b7b846a9cb6e7ae5cb46da007c1ceb6ef
Gabriel <nobrecoder@gmail.com>
2022-03-22 12:19:30 -0300

    22/03/2022

---
+++ arquivos_das_aulas/04-arquivo_php-fundamentos_de_arquivos_php.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo "Olá mundo";
+    
+    //SOBRE ARQUIVOS PHP:
+
+    /* 
+        # - Devem ser iniciados com "<?php" sempre;
+        # - Não precisam necessáriamente fechar as tags com "?>" se estiverem em arquivos externos;
+        # - Se estiverem incorporados a um arquivo html têm que usar o fechamento da tag com "?>";
+        # - Todo comando deve terminar com ";" (ponto e vírgula);
+        # - Podemos usar qualquer extensão de arquivo de texto para utilizar o php, não precisa ser necessariamente ".php"
+
+    */
+?>
