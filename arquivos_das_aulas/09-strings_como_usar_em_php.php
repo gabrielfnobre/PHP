db2f152f1568dd33e8f6f01d9121f475a09984d2
Gabriel <nobrecoder@gmail.com>
2022-03-23 08:37:39 -0300

    23/03/2022

--- arquivos_das_aulas/09-strings_como_usar_em_php.php
+++ arquivos_das_aulas/09-strings_como_usar_em_php.php
@@ -7,13 +7,20 @@
     //Aspas duplas podemos interpolar com variáveis e caracteres especiais...
     echo "eu uso interpolação $nome\n";
 
+    //Podemos concatenar strings e variáveis com o uso do "ponto"...
+    echo "Meu nome é " . $nome . " mas não da Silva\n";
+
     //Aspa única não pode interpolar com nada...
     echo 'nao uso interpolação';
 
     //Caracteres Escape com uso da contra barra...
-    echo "\n"; //Pula uma linha
-    echo "\tFaz tab";
-    echo "\v\vFaz tab vertical";
-    echo "Quebrando linha com "
-
+    echo "\n"; //Pula uma linha...
+    echo "\tFaz tab\n"; //Faz tabulação...
+    echo "\v\vFaz tab vertical\n"; //Faz tab vertical, semelhante a pular uma linha...
+    
+    //Uso da constante PHP_EOL, a constante PHP_EOL é uma constante que imita os caracteres de pular linha do sistema que estiver lendo o PHP. Essa constante torna o pulo de linha mais legível no PHP.
+    echo PHP_EOL;
+    echo PHP_EOL;
+    echo PHP_EOL;
+    echo PHP_EOL;
 ?>
