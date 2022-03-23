74f48b648b90bc06250431bc22cae1e960a483c3
Gabriel <nobrecoder@gmail.com>
2022-03-23 15:16:51 -0300

    23/03/2022

---
+++ arquivos_das_aulas/15-operador_termario.php
@@ -0,0 +1,12 @@
+<?php
+
+    //OPERADOR TERMARIO NO PHP:
+
+    $sim_ou_nao = "nao";
+
+    echo $sim_ou_nao == "sim" ? "Legal, você disse sim!\n" : "Que pena!\n";
+    
+    $sim_ou_nao = "sim";
+    
+    echo $sim_ou_nao == "sim" ? "Legal, você mudou de ideia!\n" : "Tá bom então!\n";
+?>
